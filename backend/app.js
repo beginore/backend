96fa647db09022fd455e7d83142c44137736b6b1
zhansaya231266 <154576616+zhansaya231266@users.noreply.github.com>
2025-01-19 05:41:49 +0500

    Add files via upload
    novi

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path"; 
 import { errorMiddleware } from "./middlewares/error.js";
 import reservationRouter from "./routes/reservationRoute.js";
 import { dbConnection } from "./database/dbConnection.js";
 import authRouter from "./routes/authRoute.js";
+import bcrypt from "bcrypt"
 
-const app = express();
+
 dotenv.config({ path: "./config/config.env" });
+const app = express();
+
 
 app.use(
   cors({
@@ -23,10 +27,63 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/reservation", reservationRouter);
 app.use("/api/v1/auth", authRouter);
 
-app.get("/", (req, res, next)=>{return res.status(200).json({
-  success: true,
-  message: "HELLO WORLD AGAIN"
-})})
+app.set("views", path.join(path.resolve(), "frontend", "profile", "views"));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(path.resolve(), "frontend", "profile", "public")));
+;
+
+app.get("/", (req, res)=>{
+  res.render("profile")
+})
+app.get("/signin" , (req, res) => {
+  res.render("signin");
+});
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+//register
+app.post("/signup", async (req, res) => {
+  const data = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }
+
+  //cheking if username exists
+  const existingUser = await collection.findOne({name: data.name});
+  if(existingUser) {
+    res.send("User already exists. Please choose a different username.");
+  }else {
+    const userdata = await collection.insertMany(data);
+    console.log(userdata);
+  }
+  
+})
+
+//signin
+app.post("/signin", async (req, res) => {
+  try{
+    const check = await collection.findOne({name: req.body.name});
+    if(!check) {
+      res.send("Username can not found.")
+    }
+
+    //checking password
+    const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+    if(isPasswordMatch) {
+      res.render("profile");
+    } else{
+      req.send("Wrong Password.")
+    }
+  }catch {
+    res.send("Wrong Details.")
+  }
+})
+
+
+
 
 
 console.log("MONGO_URL:", process.env.MONGO_URL);
