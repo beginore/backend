7fa701dca2fbf84dd59175c31f70ea786feb3edf
zhansaya231266 <154576616+zhansaya231266@users.noreply.github.com>
2025-01-27 16:15:52 +0500

    Add files via upload

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,20 +1,11 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    select: false, 
-  },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, select: false },
+  otp: { type: String }, // Временный код OTP
+  otpExpires: { type: Date }, // Время истечения OTP
 });
 
 export const User = mongoose.model("User", userSchema);
